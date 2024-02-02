4c54b8d711e1044729eb2488f9e2468ccd22868b
Siddhartha Singh <siddhartha.joy@gmail.com>
2024-02-02 13:23:03 +0530

    install script

---
+++ packages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+packages=(alacritty zsh hyprland xdg-desktop-portal-hyprland xdg-desktop-portal-gtk pipewire wireplumber qt5-wayland qt6-wayland polkit-gnome waybar wofi brightnessctl wl-clipboard hyprpaper blueman xwaylandvideobridge ttf-cascadia-code-nerd ttf-cascadia-mono-nerd github-cli)
+
+sudo pacman -S "${packages[@]}"
+
+aur_packages=(swaync apple-fonts asusctl supergfxctl)
+
+paru -S "${aur_packages[@]}"
