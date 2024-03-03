8b873db083ac9d91bca303e22356353d07c8a548
Siddhartha Singh <siddhartha.joy@gmail.com>
2024-03-03 21:06:47 +0530

    minor changes

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-packages=(alacritty zsh hyprland xdg-desktop-portal-hyprland xdg-desktop-portal-gtk pipewire wireplumber qt5-wayland qt6-wayland polkit-gnome waybar wofi brightnessctl wl-clipboard hyprpaper blueman xwaylandvideobridge ttf-cascadia-code-nerd ttf-cascadia-mono-nerd github-cli)
+packages=(alacritty zsh hyprland xdg-desktop-portal-hyprland xdg-desktop-portal-gtk pipewire wireplumber qt5-wayland qt6-wayland polkit-gnome waybar wofi brightnessctl wl-clipboard hyprpaper blueman xwaylandvideobridge ttf-cascadia-code-nerd ttf-cascadia-mono-nerd github-cli grim slurp)
 
 sudo pacman -S "${packages[@]}"
 
