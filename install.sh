111cdb8e8b10c0b4b4006e4f267990e57641a8f5
Siddhartha Singh <siddhartha.joy@gmail.com>
2025-03-01 00:48:50 +0530

    meh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+ln -s ~/dotfiles/ags ~/.config
 ln -s ~/dotfiles/alacritty ~/.config
 ln -s ~/dotfiles/hypr ~/.config
+ln -s ~/dotfiles/niri ~/.config
+ln -s ~/dotfiles/scripts ~/.config
+ln -s ~/dotfiles/swaync ~/.config
 ln -s ~/dotfiles/waybar ~/.config
 ln -s ~/dotfiles/wofi ~/.config
-ln -s ~/dotfiles/swaync ~/.config
-ln -s ~/dotfiles/scripts ~/.config
-ln -s ~/dotfiles/niri ~/.config
-ln -s ~/dotfiles/mimeapps.list ~/.config
 ln -s ~/dotfiles/code-flags.conf ~/.config
+ln -s ~/dotfiles/mimeapps.list ~/.config
